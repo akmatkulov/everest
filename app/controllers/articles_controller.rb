651f453fc28714408a7a68a78464699cff6285b4
akmatkulov <akmatkulovs991@gmail.com>
2022-01-31 15:25:52 +0600

    Add article create.html.er

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,23 @@
 class ArticlesController < ApplicationController
-  def new
-  end
+    def new
 
-  def create
-    render plain: params[:article].inspect
-  end
+    end
+
+    def create
+        @article = Article.new(article_params)
+        if @article.valid?
+          @article.save
+        else
+          render action: 'new'
+        end
+    end
+
+      private
+
+      def article_params
+        params.require(:article).permit(:title, :text)
+      end
+
+
+
 end
