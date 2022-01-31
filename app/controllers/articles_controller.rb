671b07e1539e6fb734bfe97feb6855aece8f6669
akmatkulov <akmatkulovs991@gmail.com>
2022-01-31 20:37:18 +0600

    Implement articles#edit and articles#updat

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,19 +14,31 @@ class ArticlesController < ApplicationController
 
     def create
         @article = Article.new(article_params)
-        if @article.valid?
-          @article.save
+        if @article.save
           redirect_to @article
         else
           render action: 'new'
         end
     end
 
-      private
+    def edit
+        @article = Article.find(params[:id])
+    end
 
-      def article_params
-        params.require(:article).permit(:title, :text)
+    def update
+      @article = Article.find(params[:id])
+      if @article.update(article_params)
+        redirect_to @article
+      else
+        render action: 'edit'
       end
+    end
+
+    private
+
+    def article_params
+        params.require(:article).permit(:title, :text)
+    end
 
 
 
